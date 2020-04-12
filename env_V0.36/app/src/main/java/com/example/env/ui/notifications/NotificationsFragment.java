857abc9d1752f4988c37418ee2d8f9cb400d13ae
huiwen99 <56622233+huiwen99@users.noreply.github.com>
2020-04-12 22:31:53 +0800

    changed actionbar/nav bar colors

--- env_V0.36/app/src/main/java/com/example/env/ui/notifications/NotificationsFragment.java
+++ env_V0.36/app/src/main/java/com/example/env/ui/notifications/NotificationsFragment.java
@@ -41,13 +41,13 @@ public class NotificationsFragment extends Fragment implements RecyclerViewItemL
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
 
         ((MainActivity) getActivity()).hideButton();
 
