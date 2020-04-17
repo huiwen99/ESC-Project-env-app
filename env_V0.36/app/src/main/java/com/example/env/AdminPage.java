ed5914b3095df0361dbd93606807f382e9fda9b2
huiwen99 <56622233+huiwen99@users.noreply.github.com>
2020-04-17 14:41:54 +0800

    adminpage updated

--- env_V0.36/app/src/main/java/com/example/env/AdminPage.java
+++ env_V0.36/app/src/main/java/com/example/env/AdminPage.java
@@ -5,10 +5,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.env.ui.home.ViewOwnListing;
 
+import java.util.ArrayList;
+
 public class AdminPage extends AppCompatActivity {
+
+    TextView bannedWordsList;
+    Button banWordsButton;
+    TextView bannedUsersList;
+    Button banUsersButton;
+    Button banListingsButton;
+
+    ArrayList<String> bannedWords = new ArrayList<String>();
+    ArrayList<String> bannedUsers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,22 @@ public class AdminPage extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //to pull from firebase?
+        bannedWords.add("damn");
+        bannedWords.add("fuck");
+        bannedWords.add("bitch");
+        bannedWords.add("bastard");
+
+        bannedWordsList = findViewById(R.id.bannedWordsList);
+        banWordsButton = findViewById(R.id.banWordsButton);
+        bannedUsersList = findViewById(R.id.bannedUsersList);
+        banUsersButton = findViewById(R.id.banUsersButton);
+        banListingsButton = findViewById(R.id.banListingsButton);
+
+        bannedWordsList.setText(listToString());
+
+        
+
 
     }
 
@@ -27,5 +56,15 @@ public class AdminPage extends AppCompatActivity {
         Intent intent = new Intent(AdminPage.this,MainActivity.class);
         startActivity(intent);
         return true;
+    }
+
+
+    private String listToString(){
+        String newString = "";
+        for(String s : bannedWords){
+            newString+=s;
+            newString+="\n";
+        }
+        return newString;
     }
 }
